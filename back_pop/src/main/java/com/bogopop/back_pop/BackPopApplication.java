ac2568b749050f4122e5fa8be8a7d3ff8e609e46
PARK SEHYUN <sehyunpark0103@gmail.com>
2024-01-14 15:06:05 +0900

    Create back_pop

---
+++ back_pop/src/main/java/com/bogopop/back_pop/BackPopApplication.java
@@ -0,0 +1,13 @@
+package com.bogopop.back_pop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackPopApplication.class, args);
+    }
+
+}
---
+++ back_pop/src/test/java/com/bogopop/back_pop/BackPopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogopop.back_pop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
