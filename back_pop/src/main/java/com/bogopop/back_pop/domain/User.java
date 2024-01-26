10901ae91232b02d1cc96813baf00ee7cb7d87ed
PARK SEHYUN <sehyunpark0103@gmail.com>
2024-01-26 16:40:13 +0900

    Update User and made joining process

--- back_pop/src/main/java/com/bogopop/back_pop/domain/User.java
+++ back_pop/src/main/java/com/bogopop/back_pop/domain/User.java
@@ -1,37 +1,47 @@
 package com.bogopop.back_pop.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity // Entity 클래스 == 테이블과 링크될 클래스
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor // Lombok 어노테이션: 기본 생성자 자동 추가
 @Getter @Setter // Lombok 어노테이션: 클래스 내 모든 필드들의 Getter, Setter 자동 생성
+@Table(name = "User")
 public class User {
     @Id // 해당 테이블의 PK 필드
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 생성을 데이터베이스에 위임 -> id 값을 따로 할당하지 않아도 데이터베이스가 자동으로 AUTO_INCREMENT 하여 기본키 생성
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false, unique = true)
     private String nickname;
+
+    @Column(nullable = false)
     private String profile;
+
+    @Column(nullable = false)
     private String background;
+
     private String liked_movies;
     private String liked_lists;
     private String my_lists;
     private String my_reviews;
-    private String generated_date;
-    private String modified_date;
 
-    public User(String nickname, String email, String password) {
-        this.nickname = nickname;
-        this.email = email;
-        this.password = password;
-    }
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime generated_date;
 
-
+    @LastModifiedDate
+    private LocalDateTime modified_date;
 }
--- back_pop/src/main/java/com/bogopop/back_pop/service/UserService.java
+++ back_pop/src/main/java/com/bogopop/back_pop/service/UserService.java
@@ -1,11 +1,37 @@
 package com.bogopop.back_pop.service;
 
+import com.bogopop.back_pop.domain.User;
+import com.bogopop.back_pop.dto.UserDto;
 import com.bogopop.back_pop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Service
+@Transactional
 public class UserService {
-    private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    public User join(UserDto userDto){
+        // 같은 이름이 있는 중복 회원 X
+//        validateDuplicateMember(user); // 중복 회원 검증
+
+        return userRepository.save(
+                User.builder()
+                        .email(userDto.getEmail())
+                        .password(userDto.getPassword())
+                        .nickname(userDto.getNickname())
+                        .profile(userDto.getProfile())
+                        .background(userDto.getBackground())
+                        .build()
+        );
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
