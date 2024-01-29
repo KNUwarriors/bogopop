e3f39cd0c700ceb2d1684df49ff6c99e8469f912
yunhee999 <yghqwer@gmail.com>
2024-01-29 10:44:23 +0900

    front back connecting

--- front_pop/src/pages/Main.js
+++ front_pop/src/pages/Main.js
@@ -1,19 +1,10 @@
 import React, { useRef, useEffect, useState } from 'react';
+import axios from 'axios'
 import './main.css';
 import { Link } from 'react-router-dom';
 
 function Main() {
-
-    const movieData = [
-        { id: 1, title: '위시', poster: '/img/poster_1.jpg', rating: 4.9 },
-        { id: 2, title: '로키', poster: '/img/poster_2.jpg', rating: 4.5 },
-        { id: 3, title: '오펜하이머', poster: '/img/poster_3.jpg', rating: 4.3 },
-        { id: 4, title: '아메리칸 셰프', poster: '/img/poster_4.jpg', rating: 4.1 },
-        { id: 5, title: '짱구', poster: '/img/poster_5.jpg', rating: 3.7 },
-        { id: 6, title: '콜바넴', poster: '/img/poster_6.jpg', rating: 3.4 },
-        { id: 7, title: '작은 아씨들', poster: '/img/poster_7.jpg', rating: 2.6 },
-    ];
-
+    const [movieData, setMovieData] = useState([]);
     const containerRef = useRef(null);
     const [isLeftButtonVisible, setIsLeftButtonVisible] = useState(true);
     const [isRightButtonVisible, setIsRightButtonVisible] = useState(true);
@@ -32,6 +23,25 @@ function Main() {
             container.removeEventListener('scroll', handleScroll);
         };
     }, []);
+
+    useEffect(() => {
+        axios.get('/movies')
+            .then((response) => {
+                // 가져온 데이터에서 이미지 URL만 추출하여 저장
+                const moviesWithImages = response.data.map(movie => ({
+                    id: movie.id,
+                    korean_title: movie.korean_title,
+                    poster_path: movie.poster_path,
+                }));
+
+                setMovieData(moviesWithImages);
+            })
+            .catch((error) => {
+                console.error('Error fetching movie data:', error);
+                setMovieData([]);
+            });
+    }, []);
+
 
     const scroll = (direction) => {
         const scrollAmount = 500; // 스크롤 양 조절 가능
@@ -59,14 +69,18 @@ function Main() {
                     <button className="scrollButton_main left" onClick={() => scroll('left')}>&lt;</button>
                 )}
                 <div className='MainPopular' ref={containerRef}>
-                    {movieData.map((movie) => (
-                        <Link key={movie.id} to={`/movies/${movie.id}`}>
-                            <div className="moviePoster">
-                                <img src={movie.poster} alt={movie.title} />
-                            </div>
-                        </Link>
-                    ))}
+                    {movieData.map((movie) => {
+                        return (
+                            <Link key={movie.id} to={`/movies/${movie.id}`}>
+                                <div className="moviePoster">
+                                    <img src={movie.poster_path} alt={movie.korean_title} />
+                                </div>
+                            </Link>
+                        );
+                    })}
+
                 </div>
+
                 {isRightButtonVisible && (
                     <button className="scrollButton_main right" onClick={() => scroll('right')}>&gt;</button>
                 )}
