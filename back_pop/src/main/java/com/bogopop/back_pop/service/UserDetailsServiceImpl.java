6fdefe48bf93d34691f63061bf7a3bb2ec6d730b
Jeongwon <cwon0737@gmail.com>
2024-01-31 13:40:26 +0900

    temp

--- back_pop/src/main/java/com/bogopop/back_pop/service/UserDetailsServiceImpl.java
+++ back_pop/src/main/java/com/bogopop/back_pop/service/UserDetailsServiceImpl.java
@@ -18,6 +18,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("userDetails");
         User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
 
