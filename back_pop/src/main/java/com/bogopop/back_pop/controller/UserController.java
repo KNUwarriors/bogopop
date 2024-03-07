14574a3272149961cd939fbf27b5f84d984268ab
PARK SEHYUN <sehyunpark0103@gmail.com>
2024-03-07 11:36:51 +0900

    add mypage function

--- back_pop/src/main/java/com/bogopop/back_pop/controller/UserController.java
+++ back_pop/src/main/java/com/bogopop/back_pop/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.bogopop.back_pop.controller;
 
+import com.bogopop.back_pop.domain.MovieLike;
+import com.bogopop.back_pop.domain.Review;
 import com.bogopop.back_pop.domain.User;
 import com.bogopop.back_pop.dto.UserDto;
 import com.bogopop.back_pop.dto.TokenDto;
+import com.bogopop.back_pop.service.LikeService;
+import com.bogopop.back_pop.service.ReviewService;
 import com.bogopop.back_pop.service.UserService;
 import com.bogopop.back_pop.jwt.TokenProvider;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +22,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
 public class UserController {
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+    private final LikeService likeService;
+    private final ReviewService reviewService;
 
     private final TokenProvider tokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
@@ -78,7 +84,7 @@ public class UserController {
 
     @GetMapping("/user")
     @ApiOperation("현재 로그인된 사용자 정보 조회")
-    public ResponseEntity<User> getCurrentUser() {
+    public ResponseEntity<Map<String, Object>> getCurrentUser() {
         // 현재 인증된 사용자의 정보를 가져오는 코드를 작성해야 합니다.
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String userEmail = authentication.getName(); // 현재 로그인된 사용자의 이메일을 가져옴
@@ -88,21 +94,16 @@ public class UserController {
             // 사용자를 찾을 수 없는 경우 404 에러를 반환합니다.
             return ResponseEntity.notFound().build();
         }
-    // User 객체를 UserDto 객체로 변환하여 반환합니다.
-        UserDto userDto = UserDto.builder()
-                .email(user.getEmail())
-                .password(user.getPassword())
-                .nickname(user.getNickname())
-                .profile(user.getProfile())
-                .background(user.getBackground())
-                .role(user.getRole())
-                .liked_movies(user.getLiked_movies())
-                .liked_lists(user.getLiked_lists())
-                .my_lists(user.getMy_lists())
-                .my_reviews(user.getMy_reviews())
-                .build();
-        return ResponseEntity.ok(user);
-    }
 
+        List<MovieLike> movieLikes = likeService.getAllMovieLikeByUserId(user.getId());
+        List<Review> reviews = reviewService.getAllByUserId(user.getId());
+
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("user", user);
+        userData.put("movieLikes", movieLikes);
+        userData.put("reviews", reviews);
+
+        return ResponseEntity.ok(userData);
+    }
 
 }
