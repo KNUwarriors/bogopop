87a0009c76ca2b67e00dbf4dda5126b843b91f43
yunhee999 <yghqwer@gmail.com>
2024-03-15 17:43:17 +0900

    main popular movies

--- front_pop/src/pages/Main.js
+++ front_pop/src/pages/Main.js
@@ -3,9 +3,7 @@ import axios from 'axios'
 import './main.css';
 import { Link } from 'react-router-dom';
 
-function Main() {
-    const [movieData, setMovieData] = useState([]);
-    const [randomMovie, setRandomMovie] = useState(null); // 추가된 부분, 메인페이지 랜덤배너
+const MainContainer = ({ title, movieData }) => {
     const containerRef = useRef(null);
     const [isLeftButtonVisible, setIsLeftButtonVisible] = useState(true);
     const [isRightButtonVisible, setIsRightButtonVisible] = useState(true);
@@ -25,10 +23,52 @@ function Main() {
         };
     }, []);
 
+    const scroll = (direction) => {
+        const scrollAmount = 500;
+        const currentScroll = containerRef.current.scrollLeft;
+
+        if (direction === 'left') {
+            containerRef.current.scrollLeft = Math.max(currentScroll - scrollAmount, 0);
+        } else if (direction === 'right') {
+            containerRef.current.scrollLeft = Math.min(
+                currentScroll + scrollAmount,
+                containerRef.current.scrollWidth - containerRef.current.clientWidth
+            );
+        }
+    };
+
+    return (
+        <div className='main-container'>
+            <h2>{title}</h2>
+            <hr />
+            {isLeftButtonVisible && (
+                <button className="scrollButton left" onClick={() => scroll('left')}>&lt;</button>
+            )}
+            <div className='movies' ref={containerRef}>
+                {movieData.map((movie) => {
+                    return (
+                        <Link key={movie.id} to={`/movies/${movie.id}`}>
+                            <div className="moviePoster">
+                                <img src={movie.poster_path} alt={movie.korean_title} />
+                            </div>
+                        </Link>
+                    );
+                })}
+            </div>
+            {isRightButtonVisible && (
+                <button className="scrollButton right" onClick={() => scroll('right')}>&gt;</button>
+            )}
+        </div>
+    );
+};
+
+function Main() {
+    const [movieData, setMovieData] = useState([]);
+    const [randomMovie, setRandomMovie] = useState(null);
+
     useEffect(() => {
-        axios.get('/movies')
+        axios.get('/movies/popularMovies')
             .then((response) => {
-                // 가져온 데이터에서 이미지 URL만 추출하여 저장
                 const moviesWithImages = response.data.map(movie => ({
                     id: movie.id,
                     korean_title: movie.korean_title,
@@ -46,19 +86,9 @@ function Main() {
                 console.error('Error fetching movie data:', error);
                 setMovieData([]);
             });
+
     }, []);
 
-
-    const scroll = (direction) => {
-        const scrollAmount = 500; // 스크롤 양 조절 가능
-        const currentScroll = containerRef.current.scrollLeft;
-
-        if (direction === 'left') {
-            containerRef.current.scrollLeft = Math.max(currentScroll - scrollAmount, 0);
-        } else if (direction === 'right') {
-            containerRef.current.scrollLeft = Math.min(currentScroll + scrollAmount, containerRef.current.scrollWidth);
-        }
-    };
 
     return (
         <div>
@@ -70,28 +100,8 @@ function Main() {
                 </Link>
             )}
 
-            <h1>이번 주 인기 영화!</h1>
-
-            <div className='MainPopularWrapper'>
-                {isLeftButtonVisible && (
-                    <button className="scrollButton_main left" onClick={() => scroll('left')}>&lt;</button>
-                )}
-                <div className='MainPopular' ref={containerRef}>
-                    {movieData.map((movie) => {
-                        return (
-                            <Link key={movie.id} to={`/movies/${movie.id}`}>
-                                <div className="moviePoster">
-                                    <img src={movie.poster_path} alt={movie.korean_title} />
-                                </div>
-                            </Link>
-                        );
-                    })}
-
-                </div>
-
-                {isRightButtonVisible && (
-                    <button className="scrollButton_main right" onClick={() => scroll('right')}>&gt;</button>
-                )}
+            <div>
+                <MainContainer title='이번 주 인기 영화' movieData={movieData} />
             </div>
         </div>
     );
